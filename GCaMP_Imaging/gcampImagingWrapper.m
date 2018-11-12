fd4fe27d9a83984fcc54d3525d9ec7d4bbbe5560
kk1995 <b.kim@email.wustl.edu>
2018-11-12 16:42:04 -0600

    Update GCaMP_Imaging/gcampImagingWrapper.m

--- GCaMP_Imaging/gcampImagingWrapper.m
+++ GCaMP_Imaging/gcampImagingWrapper.m
@@ -6,8 +6,8 @@
 
 %% state the tiff file
 
-% tiffFileName = "L:\181031-GCampM2-fc1.tif";
-tiffFileName = "J:\180813\180813-ProbeW3M2-Post.tif";
+tiffFileName = "L:\181031-GCampM2-fc1.tif";
+% tiffFileName = "J:\180813\180813-ProbeW3M2-Post.tif";
 
 %% state where the led spectrum files and extinction coefficient files are
 
@@ -24,24 +24,35 @@ extCoeffDir = "C:\Repositories\GitHub\OIS\Spectroscopy\";
 % for sessionInfo, you need framerate, freqout, lowpass, and highpass
 
 % systemType = 'fcOIS1', 'fcOIS2', 'fcOIS2_Fluor' or 'EastOIS1_Fluor'
-systemInfo = mouse.expSpecific.sysInfo('fcOIS2_Fluor');
+systemInfo = mouse.expSpecific.sysInfo('EastOIS1_Fluor');
 
 % sessionType = 'fc' or 'stim'
-sessionInfo = mouse.expSpecific.session2procInfo('fc');
+sessionInfo = mouse.expSpecific.session2procInfo('stim');
+sessionInfo.framerate = 16.8;
 sessionInfo.freqout = 2;
 
 %% get gcamp and hb data
 
-% [raw, time, xform_hb, xform_gcamp, xform_gcampCorr, isbrain, xform_isbrain, markers] ...
-%     = gcampImaging(tiffFileName, systemInfo, sessionInfo, ledDir, extCoeffDir);
-
-% if brain mask and markers are available:
 [raw, time, xform_hb, xform_gcamp, xform_gcampCorr, isbrain, xform_isbrain, markers] ...
-    = gcampImaging(tiffFileName, systemInfo, sessionInfo, ledDir, extCoeffDir, ...
-    isbrain, markers);
+    = gcampImaging(tiffFileName, systemInfo, sessionInfo, ledDir, extCoeffDir);
+
+% % if brain mask and markers are available:
+% [raw, time, xform_hb, xform_gcamp, xform_gcampCorr, isbrain, xform_isbrain, markers] ...
+%     = gcampImaging(tiffFileName, systemInfo, sessionInfo, ledDir, extCoeffDir, ...
+%     isbrain, markers);
 
 % isbrain = logical nxn array of brain mask.
 % markers = the brain markers that are created during the whole GUI where
 % you click on the midline suture and lambda. If you do not have these,
 % just run the code without giving these inputs, go through the GUI, then
 % the code will output isbrain, xform_isbrain, and markers.
+
+%% plot
+
+plot(time,squeeze(xform_hb(79,95,1,:))*1000);
+hold on; plot(time,squeeze(xform_hb(79,95,2,:))*1000);
+hold on; plot(time,squeeze(xform_gcamp(79,95,1,:)));
+hold on; plot(time,squeeze(xform_gcampCorr(79,95,1,:)));
+hold off;
+
+legend('HbO (mM)','HbR (mM)','gcamp','gcamp corrected');
